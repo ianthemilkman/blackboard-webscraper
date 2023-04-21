4d2b6be47632399498bf6a018000b80dbf3ce0f2
Ian <imilko007@gmail.com>
2023-04-20 23:28:12 -0400

    got the grades working properly i think

--- grade_scraper.py
+++ grade_scraper.py
@@ -1,12 +1,15 @@
 
 import os
 from dotenv import load_dotenv
+import traceback
+import sys
 from time import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -22,6 +25,8 @@ driver.get("https://blackboard.gwu.edu/webapps/login/")
 
 load_dotenv()
 
+x = 0
+
 def loginPage():
     username = os.getenv("BB_USERNAME")
     password = os.getenv("BB_PASSWORD")
@@ -32,14 +37,15 @@ def loginPage():
     user_field.send_keys(username)
     pass_field.send_keys(password)
     login_btn.click()
-    print("success.")
+    print("Login: success.")
 
 def cookieAgree():
     wait = WebDriverWait(driver, 10)
     ok_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[7]/div/div/div/div/div/div/div[2]/button")))
 
     ok_btn.click()
-    print("success.")
+    print("Cookie agree: success.")
+    print("-----------------------------------------------\nGRADES: \n")
 
 def openCourse():
     wait = WebDriverWait(driver, 10)
@@ -50,26 +56,30 @@ def openCourse():
         formatted_name = formatName(name.text)
         next_course.click()
         course_grade = getCourseGrade()
-        print(f"'{formatted_name}' grade:", course_grade)
+#         print(course_grade)
+        if course_grade[1] != "Total":
+            print(f"'{formatted_name}' grade: TOTAL NOT AVAILABLE")
+        else:
+            print(f"'{formatted_name}' grade:", course_grade[0].replace('\n', ''))
         courses_btn = driver.find_element(By.XPATH, '//*[@id="Courses.label"]/a')
         courses_btn.click()
-    print("success.")
 
 def getCourseGrade():
     wait = WebDriverWait(driver, 3)
     try:
         show_grades = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'My Grades')))
         show_grades.click()
-        grade = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[3]/div/div/div/div/div[2]/div[3]/div[2]/div[1]/div[3]/span')))
-        print("success.")
-        return grade.text
-    except Exception:
+        grade_type = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[3]/div/div/div/div/div[2]/div[3]/div[2]/div[2]/div[1]/span')))
+        tot_grade = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[3]/div/div/div/div/div[2]/div[3]/div[2]/div[2]/div[3]')))
+        return [tot_grade.text, grade_type.text]
+    except TimeoutException:
+#         traceback.print_exc(file=sys.stdout)
         return "Not found"
 
 def formatName(name):
-    new_name = name[21:]
-    new_name = new_name[:-13]
-    return(new_name)
+    split_name = name.split("_")
+    formatted_name = split_name[2]
+    return(formatted_name)
 
 def main():
     loginPage()
@@ -82,5 +92,5 @@ if __name__ == '__main__':
     start = float(time())
     main()
     end = float(time())
-    print('completed in:', (end-start))
+    print('-----------------------------------------------\ncompleted in:', (end-start))
 
